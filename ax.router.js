220494b9bc34f7c159c1d5aad596d859b37ca60b
Goodwish Matyila <goomatyila@gmail.com>
2021-07-01 19:22:51 +0200

    Update ax.router.js
    ESNext version

--- ax.router.js
+++ ax.router.js
@@ -5,84 +5,43 @@
  * @Version 0.0.1
  * @Author Goodwish Matyila
  */
-
-/**
- * @description listens to the hashchange event then makes an update to the view.
- * @params window, document
- */
-const browserDocumentObject = document;
-const browserWindowObject = window;
-const urlLocation = new function () {
-     this.url = location,
-          this.getTemplate = async function (templateNameWithFullPath, elementRefCssSelector) {
-               let acquiredTemplate = await fetch(templateNameWithFullPath);
-               let templateData = await acquiredTemplate.text();
-               return templateData;
-          }
+export class RouterConfig {
+     render(templateRef) {
+         
+          this.node = document.importNode(templateRef, true);
+          this.preloader()
+          this.updateAppContent(this.node); 
+     }
+     getTemplate(id) {
+          return document.getElementById(id).content;
+     }
+     clearAndUpdate() {
+          let content = document.querySelector(this.content);
+          content.innerHTML = '';
+          return document.querySelector(this.content);
+     }
+     preloader() {
+          history.pushState({}, '',  location.hash.substring(1).trim());
+          let loader = document.importNode(this.getTemplate('preloader'), true);
+          this.clearAndUpdate()
+          document.querySelector(this.content).append(loader);
+     }
+    
+     updateAppContent(contentView) {
+          setTimeout(()=>{
+               this.clearAndUpdate().append(contentView);
+          }, 5000 ) //randomize this
+     }
+     constructor() {
+          this.content = "#content";
+          addEventListener('hashchange', () => {
+               if (location.hash.substring(1) == "/") {
+                    this.render(this.getTemplate('_home')) //_home is the id of a <template id="_home"></template> 
+               }
+          })
+     }
 }
 
 
-/* Attach Event */
-browserWindowObject.addEventListener('hashchange', routesConfig.bind(urlLocation));
-
-//set initial location path
+var routes = new RouterConfig();
 location.replace("#/");
-
- 
-function routesConfig() {
-
-     function registerRoute(requestUrl, filePath,  container){
-          if (hash.match(`#/${requestUrl}`)) {
-               this.getTemplate(filePath)
-                    .then(data => document.querySelector(container).innerHTML = data)
-          }else{
-               document.querySelector(container).innerHTML = "404 not found";
-          }
-     }
-
-     /**
-      * @params takes the full path of the template and filename
-      */
-     let {
-          hash
-     } = this.url;
-
-     /* Write your routes here using conditionals  */
-     if (hash.match("#/social-media-marketing")) {
-          this.getTemplate("views/social-imc.html")
-               .then(data => document.querySelector("#main").innerHTML = data)
-     }
-     if (hash.match("#/privacy")) {
-          this.getTemplate("views/privacy.html")
-               .then(data => document.querySelector("#main").innerHTML = data)
-     }
-     if (hash.match("#/disclaimer")) {
-          this.getTemplate("views/disclaimer.html")
-               .then(data => document.querySelector("#main").innerHTML = data)
-     }
-     if (hash.match("#/terms")) {
-          this.getTemplate("views/terms.html")
-               .then(data => document.querySelector("#main").innerHTML = data)
-     }
-     if (hash.match("#/process")) {
-          this.getTemplate("views/process.html")
-               .then(data => document.querySelector("#main").innerHTML = data)
-     }
-     if (hash.match("#/contact")) {
-          this.getTemplate("views/contact.html")
-               .then(data => document.querySelector("#main").innerHTML = data)
-     }
-     if (hash.match("#/")) {
-          this.getTemplate("views/home.html")
-               .then(data => document.querySelector("#main").innerHTML = data)
-     }
-     if (hash.match("#/services")) {
-          this.getTemplate("views/services.html")
-               .then(data => document.querySelector("#main").innerHTML = data)
-     }
-     if (hash.match("#/pricing")) {
-          this.getTemplate("views/pricing.html")
-               .then(data => document.querySelector("#main").innerHTML = data)
-     }
-     
-}
